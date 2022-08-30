5c9398b323d808b0f8ed40478fad8d7da01fa252
Andraš Tšitškan <36357686+andras-tsitskan@users.noreply.github.com>
2022-08-30 21:53:50 +0300

    Add cell marking

--- javascript.js
+++ javascript.js
@@ -10,10 +10,30 @@ const human = playerFactory("X");
 const computer = playerFactory("O");
 
 const gameboard = (() => {
+  // SELECTORS
+  const cells = document.querySelectorAll(".js-gameboard-cell");
   // INIT
   const gameboard = new Array(9);
   resetGameboard();
 
+
+  // EVENT FUNCTIONS
+  function markCell(event) {
+    if (gameboard[event.target.dataset.cell] !== human.getMarker()) {
+      gameboard[event.target.dataset.cell] = human.getMarker();
+      event.target.textContent = human.getMarker();
+      console.log(gameboard);
+    }
+  }
+
+  // EVENT BINDING
+  // addEventToCells() is created as a function for the possibility to call it in resetGame().
+  const addEventToCells = () => {
+    cells.forEach((cell) => {
+      cell.addEventListener("click", markCell);
+    });
+  };
+  addEventToCells();
   // OTHER FUNCTIONS
   function resetGameboard() {
     gameboard.fill("");
